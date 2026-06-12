91ba0db80d97228193aeaea8fe86d21adc13d5c9
eblondel <emmanuel.blondel1@gmail.com>
2026-06-12 08:52:09 +0200

    improve packaging

---
+++ R/iotc.base.form.management.R
@@ -0,0 +1,16 @@
+#' @name iotc.base.form.management
+#' @aliases iotc.base.form.management-package
+#' @aliases iotc.base.form.management
+#'
+#' @import data.table
+#' @import lubridate
+#' @import openxlsx
+#' @import stringr
+#' @import iotc.core.utils.misc
+#' @import iotc.data.reference.codelists
+#'
+#' @title iotc.base.form.management
+#' @description iotc.base.form.management
+
+#'
+"_PACKAGE"
